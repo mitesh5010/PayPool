27693ffa37d8a07f6cf3eaf474e1dfc3c0567faf
Mitesh <miteshprajapati6439@gmail.com>
2025-06-26 13:56:59 +0530

    Create login component with validation

---
+++ src/app/auth/login/login.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [ReactiveFormsModule],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent implements OnInit{
+
+  loginForm!: FormGroup;
+  showPassword= false;
+  
+  constructor(private fb:FormBuilder){}
+  ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password:['', [Validators.required, Validators.minLength(4)]]
+    })
+  }
+
+  togglePasswordVisibility(): void {
+    this.showPassword = !this.showPassword;
+  }
+
+  onSubmit(){
+    if (this.loginForm.invalid) {
+      this.loginForm.markAllAsTouched();
+      return;
+    }
+
+    const {email, password} = this.loginForm.value;
+
+    console.log(this.loginForm.value);
+    
+  }
+
+  get email() {
+    return this.loginForm.get('email');
+  }
+
+  get password() {
+    return this.loginForm.get('password');
+  }
+  
+}
