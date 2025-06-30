5f4dcff38a5d23d84bc96e63d4aef2173c1185d5
Mitesh <miteshprajapati6439@gmail.com>
2025-06-30 17:32:10 +0530

    optimized the sidebar component and set navbar-links

--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -3,13 +3,20 @@ import { Component } from '@angular/core';
 import { MenuItem } from 'primeng/api';
 import { SidebarModule } from 'primeng/sidebar';
 import { MenuModule } from 'primeng/menu';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [SidebarModule, CommonModule, MenuModule],
+  imports: [SidebarModule, CommonModule, MenuModule, RouterLinkActive, RouterLink],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css'
+  styleUrl: './sidebar.component.css',
 })
 export class SidebarComponent {
 
+  navLinks = [
+  { title: 'Dashboard', path: '/dashboard', exact: true },
+  { title: 'Expenses', path: '/dashboard/expenses' },
+  { title: 'Groups', path: '/dashboard/groups' },
+  { title: 'Settlements', path: '/dashboard/settlement' }
+];
 }
