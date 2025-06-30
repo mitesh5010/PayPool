0070d33d8762e58203e1afe205186c3a7d33c52b
Mitesh <miteshprajapati6439@gmail.com>
2025-06-30 17:12:30 +0530

    create others component for user

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,22 +1,13 @@
 import { Component } from '@angular/core';
-import { ToolbarModule } from 'primeng/toolbar';
-import { ButtonModule } from 'primeng/button';
-import { CardModule } from 'primeng/card';
-import { ChartModule } from 'primeng/chart';
-import { ListboxModule } from 'primeng/listbox';
 import { SidebarComponent } from "../shared/sidebar/sidebar.component";
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ToolbarModule, ButtonModule, CardModule, ChartModule, ListboxModule, SidebarComponent],
+  imports: [ SidebarComponent, RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent {
-  groups = [
-    { name: 'Group 1' },
-    { name: 'Group 2' },
-    { name: 'Group 3' }
-  ];
 }
