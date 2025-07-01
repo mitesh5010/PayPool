198ae0a08c34dbe9d71f8f688a96e6120b630da5
mitesh5010 <79757565+mitesh5010@users.noreply.github.com>
2025-07-01 13:20:01 +0530

    User-dashboard (#3)
    * store user info in localstorage
    
    * create overlay action content icon for user info and add loginGuard
    
    * done user dashboard design

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,14 +1,41 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { SidebarComponent } from "../shared/sidebar/sidebar.component";
 import { RouterOutlet } from '@angular/router';
+import { AuthService } from '../auth/auth.service';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [ SidebarComponent, RouterOutlet],
+  imports: [ SidebarComponent, RouterOutlet, AvatarModule,
+  OverlayPanelModule,
+  ButtonModule,
+  CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css',
   encapsulation: ViewEncapsulation.None
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  user!:{name:string, id:number, email:string};
+  constructor(private auth: AuthService){
+  }
+  ngOnInit(): void {
+    this.user = this.auth.userSignal() || JSON.parse(localStorage.getItem('user')!); 
+  }
+  logout(){
+    this.auth.logout();
+  }
+  getInitials(name: string): string {
+  if (!name) return '';
+  
+  return name
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase())
+    .slice(0, 2) // Take only first 2 initials
+    .join('');
+}
+  
 }
