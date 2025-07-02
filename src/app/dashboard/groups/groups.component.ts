6c6d89b90848d1e663c0c4b71e3bbae22b20e55a
mitesh5010 <79757565+mitesh5010@users.noreply.github.com>
2025-07-02 11:12:48 +0530

    fix primeflex and create group design (#5)

--- src/app/dashboard/groups/groups.component.ts
+++ src/app/dashboard/groups/groups.component.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, ViewEncapsulation } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+import { CardModule } from 'primeng/card';
+import {  MenuModule } from 'primeng/menu';
+import { DividerModule } from 'primeng/divider';
+
+export interface Group {
+  id: number;
+  name: string;
+  status: 'ACTIVE' | 'SETTLED';
+  members: { name: string; email: string }[];
+  total: number;
+  youOwe: number;
+  owedToYou: number;
+}
 
 @Component({
   selector: 'app-groups',
-  imports: [],
+  imports: [ButtonModule, MenuModule, CommonModule, CardModule, DividerModule],
   templateUrl: './groups.component.html',
-  styleUrl: './groups.component.css'
+  styleUrl: './groups.component.css',
+  encapsulation: ViewEncapsulation.None
+
 })
 export class GroupsComponent {
+  groups: Group[] = [];
 
+  ngOnInit() {
+    // Replace with real service call
+    this.groups = [
+      {
+        id: 1,
+        name: 'Weekend Trip',
+        status: 'ACTIVE',
+        members: [{ name: 'A', email: '' }, { name: 'B', email: '' }],
+        total: 856,
+        youOwe: 142,
+        owedToYou: 28
+      },
+      {
+        id: 2,
+        name: 'sunday Trip',
+        status: 'SETTLED',
+        members: [{ name: 'A', email: '' }, { name: 'B', email: '' }],
+        total: 578,
+        youOwe: 142,
+        owedToYou: 236
+      },
+    ];
+  }
+
+  viewGroup(id: number) {
+    // Navigate to group details
+  }
+
+  addExpense(id: number) {
+    // Open add expense dialog/modal
+  }
 }
