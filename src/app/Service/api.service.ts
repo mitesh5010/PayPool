0c8d8d63b9d9667ea5d144799f0bd7af4634c5cb
mitesh5010 <79757565+mitesh5010@users.noreply.github.com>
2025-07-03 11:27:34 +0530

    Expenses (#9)
    * create design for expenses section
    
    * fix primeflex and create group design (#5)
    
    * Make expense component interective with server
    
    * Merge branch 'expenses' of https://github.com/mitesh5010/PayPool into expenses

--- src/app/Service/api.service.ts
+++ src/app/Service/api.service.ts
@@ -1,10 +1,15 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Expense } from '../dashboard/expenses/expenses.component';
 
 export interface User {
   readonly id: number;
   readonly name: string;
+}
+export interface Category{
+  category:string;
+  id:number;
 }
 
 @Injectable({
@@ -18,4 +23,16 @@ export class ApiService {
   getAllUsers(): Observable<readonly User[]>{
     return this.http.get<readonly User[]>(`${this.apiUrl}/users`);
   }
+
+  getCategories(){
+    return this.http.get<Category[]>(`${this.apiUrl}/categories`);
+  }
+
+  getAllExpenses() {
+  return this.http.get<Expense[]>(`${this.apiUrl}/expenses`);
+}
+
+  addExpense(expense: any): Observable<any> {
+  return this.http.post(`${this.apiUrl}/expenses`, expense);
+}
 }
