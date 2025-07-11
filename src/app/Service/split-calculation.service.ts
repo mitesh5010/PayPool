6e919c080cde23080696a9e091049f730a0d2dbc
Mitesh <miteshprajapati6439@gmail.com>
2025-07-11 14:20:05 +0530

    manage stats of users with forkJoin

---
+++ src/app/Service/split-calculation.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { Expense, Group } from './data.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SplitCalculationService {
+
+  constructor() { }
+
+  calculateGroupStats(expenses: Expense[], group:Group, userId:number){
+    let total = 0;
+    let youOwe = 0;
+    let owedToYou = 0;
+    if (!expenses || !group || !group.members) {
+    return { total: 0, youOwe: 0, owedToYou: 0 };
+  }
+
+    const groupExpenses = expenses.filter(e => e.selectedGroup === group.name);
+
+    groupExpenses.forEach(exp => {
+      total += exp.amount;
+
+      const splitTotal = exp.splitDetails.reduce((sum, s) => sum + s.amount, 0);
+      const userSplit = exp.splitDetails.find(s => s.id === userId)?.amount || 0;
+
+      // You didn't pay, but owe part of it
+      if (userSplit > 0 && group.userId !== userId) {
+        youOwe += userSplit;
+      }
+      // You paid (you are the creator), others owe you
+      if (group.userId === userId) {
+        exp.splitDetails.forEach(split => {
+          if (split.id !== userId) {
+            owedToYou += split.amount;
+          }
+        });
+      }
+    });
+    return {total, youOwe, owedToYou};
+  }
+}
--- src/app/dashboard/groups/groups.component.ts
+++ src/app/dashboard/groups/groups.component.ts
@@ -18,6 +18,8 @@ import { AddExpenseDialogComponent } from '../expenses/add-expense-dialog/add-ex
 import { Expense, Group, User } from '../../Service/data.model';
 import { AuthService } from '../../auth/auth.service';
 import { ViewGroupComponent } from "./view-group/view-group.component";
+import { SplitCalculationService } from '../../Service/split-calculation.service';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-groups',
@@ -50,42 +52,62 @@ export class GroupsComponent implements OnInit {
   userId: number = 0;
   allExpenses!:Expense[];
 
-  constructor(private fb: FormBuilder, private apiService: ApiService, private auth: AuthService) {}
+  constructor(private fb: FormBuilder, private apiService: ApiService, private auth: AuthService, private splitCal: SplitCalculationService) {}
 
   ngOnInit() {
     // Replace with real service call
     this.userId = this.auth.getUserId();
-    this.apiService.getAllGroups().subscribe({
-      next: (data) => {
-        const allGroups = data.slice().reverse();
-        this.groups = allGroups.filter(
-          (group) => group.userId === this.userId || group.members.some((m) => m.id === this.userId)
-        );
-      },
-      error: (err) => {
-        console.error('Failed to load groups:', err);
-      },
-    });
-    this.apiService.getAllExpenses().subscribe({
-      next: data => {
-        this.allExpenses = data
-      },
-      error: err => console.log('error! for expenses get:',err)
+    // this.apiService.getAllGroups().subscribe({
+    //   next: (data) => {
+    //     const allGroups = data.slice().reverse();
+    //     this.groups = this.filterUserGroups(allGroups,this.userId).map(group =>{
+    //       const {total, youOwe, owedToYou } = this.splitCal.calculateGroupStats(this.allExpenses, group, this.userId)
+    //       return { ...group, total, youOwe, owedToYou}
+    //     })
+    //   },
+    //   error: (err) => {
+    //     console.error('Failed to load groups:', err);
+    //   },
+    // });
+    // this.apiService.getAllExpenses().subscribe({
+    //   next: data => {
+    //     this.allExpenses = data
+    //   },
+    //   error: err => console.log('error! for expenses get:',err)
       
-    })
-    this.apiService.getAllUsers().subscribe({
-      next: (data) => {
-        this.allMembers = data;
-      },
-      error: (err) => {
-        console.error('Failed to load users:', err);
-      },
-    });
+    // })
+    // this.apiService.getAllUsers().subscribe({
+    //   next: (data) => {
+    //     this.allMembers = data;
+    //   },
+    //   error: (err) => {
+    //     console.error('Failed to load users:', err);
+    //   },
+    // });
     this.newGroup = this.fb.group({
       name: ['', Validators.required],
       description: [''],
       members: [[], Validators.required],
     });
+
+    forkJoin({
+      users: this.apiService.getAllUsers(),
+      allExpenses: this.apiService.getAllExpenses(),
+      groups: this.apiService.getAllGroups(),
+    }).subscribe({
+      next: ({users, allExpenses, groups})=>{
+        this.allMembers = users;
+        this.allExpenses = allExpenses;
+
+        const allGroups = groups.slice().reverse();
+
+        this.groups = this.filterUserGroups(allGroups, this.userId).map(group => {
+        const stats = this.splitCal.calculateGroupStats(allExpenses, group, this.userId);
+        return { ...group, ...stats };
+      });
+      },
+      error: err => console.error('Failed to load data:', err)
+    })
   }
   filterUserGroups(allGroups: Group[], userId: number): Group[] {
   return allGroups.filter(group =>
